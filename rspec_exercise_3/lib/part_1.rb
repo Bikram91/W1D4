f1afbfe330dd5b5dba4682923b0bc56594952dc0
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-07-11 09:01:37 -0700

    updation

--- rspec_exercise_3/lib/part_1.rb
+++ rspec_exercise_3/lib/part_1.rb
@@ -1,33 +1,32 @@
-def is_prime?(num)
-    if num < 2
-        return false
-    end
+def is_prime?(number)
+  if number < 2
+    return false
+  end
 
-    (2...num).each do |factor|
-        if num % factor == 0
-            return false
-        end
-    end
-        true
+  (2...number).each do |factor|
+    return false if number % factor == 0
+  end
+  true
 end
 
-
-def prime_range(num1,num2)
-    arr = []
-    (num1..num2).each do |num3|
-        arr << num3 if is_prime?(num3)
+def nth_prime(n)
+  arr = []
+  i = 2
+  while arr.length < n
+    if is_prime?(i)
+      arr << i
     end
-    arr
+    i += 1
+  end
+  arr[-1]
 end
 
-def nth_prime(num4)
-    arr = []
-     i = 2
-    while arr.length < num4
-        if is_prime?(i)
-            arr << i     
-        end  
-        i+=1   
+def prime_range(num1, num2)
+  arr = []
+  (num1...num2+1).each do |factor|
+    if is_prime?(factor)
+      arr << factor
     end
-    arr[-1]
+  end
+  arr
 end
