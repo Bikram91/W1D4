c537f563e4acb7dea4c5e9249c19d53d2b1cb033
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-07-11 12:27:28 -0700

    updation

--- rspec_exercise_4/lib/part_1.rb
+++ rspec_exercise_4/lib/part_1.rb
@@ -1,50 +1,46 @@
 def my_reject(arr, &prc)
-    new_arr = []
-    arr.each do |ele|
-        if !(prc.call(ele))
-            new_arr << ele
-        end
-    end
-    new_arr
+  new_arr = []
+  arr.each do |ele|
+    new_arr << ele if !(prc.call(ele))
+  end
+  new_arr
 end
 
 def my_one?(arr, &prc)
-    new_arr = []
-    arr.each do |ele|
-        if prc.call(ele)
-            new_arr << ele
-        end
+  count = 0
+  arr.each do |ele|
+    if prc.call(ele)
+      count += 1
     end
-    return true if new_arr.length == 1
-    return false
+  end
+  return true if count == 1
+  false
 end
 
 def hash_select(hash, &prc)
-    new_hash = {}
-    hash.each do |k,v|
-        if prc.call(k,v)
-            new_hash[k] = v
-        end
+  new_hash = {}
+  hash.each do |k, v|
+    if prc.call(k, v)
+      new_hash[k] = v
     end
-    new_hash
+  end
+  new_hash
 end
 
 def xor_select(arr, prc1, prc2)
-    new_arr = []
-    arr.each do |ele|
-        if (!(prc1.call(ele)) || !(prc2.call(ele))) && ((prc1.call(ele)) || (prc2.call(ele)))
-            new_arr << ele
-        end
+  new_arr = []
+  arr.select do |ele|
+    if (prc1.call(ele) || prc2.call(ele)) && (!(prc1.call(ele)) || !(prc2.call(ele)))
+      new_arr << ele
     end
-    new_arr
+  end
+  new_arr
 end
 
-def proc_count(num, arr)
-    count = 0
-    arr.each do |prc|
-        if prc.call(num)
-            count +=1
-        end
-    end
-    count
+def proc_count(val, arr)
+    count = 0 
+  arr.each do |prc|
+    count += 1 if prc.call(val)
+  end
+  count 
 end
--- rspec_exercise_4/lib/part_2.rb
+++ rspec_exercise_4/lib/part_2.rb
@@ -1,33 +1,25 @@
 def proper_factors(num)
-    arr = []
-    (1...num).each do |factor|
-        if num % factor == 0
-            arr << factor
-        end
-    end
-    arr
+  arr = []
+  (1...num).each do |factor|
+    arr << factor if num % factor == 0
+  end
+  arr
 end
 
 def aliquot_sum(num)
-    proper_factors(num).sum
+  (proper_factors(num)).sum
 end
 
 def perfect_number?(num)
-    if aliquot_sum(num) == num
-        return true
-    else
-        return false
-    end
+  return true if num == aliquot_sum(num)
+  false
 end
 
-def ideal_numbers(num)
-    i = 3
-    arr = []
-    while arr.length < num
-        if perfect_number?(i)
-            arr << i
-        end
-      i+=1
-    end
-        arr
-    end
+def ideal_numbers(n)
+  arr = []
+  while arr.length <= n
+    perfect_number?(n)
+    n+=1
+  end
+  arr
+end
