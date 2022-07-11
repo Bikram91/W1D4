63ca59f1bfc11b2b345083df6a0618d2719819b4
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-07-11 09:15:20 -0700

    updation

--- rspec_exercise_3/lib/part_2.rb
+++ rspec_exercise_3/lib/part_2.rb
@@ -1,20 +1,20 @@
 def element_count(arr)
-    hash = Hash.new(0)
-    arr.each do |ele|
-        hash[ele]+=1
-    end
-    hash
+  count = Hash.new(0)
+  arr.each do |word|
+    count[word] += 1
+  end
+  count
 end
 
-def char_replace!(str, hash1)
-    str.each_char.with_index do |char, i|
-        if hash1.has_key?(char)
-           str[i] = hash1[char]
-        end
+def char_replace!(str, hash)
+  str.each_char.with_index do |char, i|
+    if hash.has_key?(char)
+      str[i] = hash[char]
     end
-    str
+  end
+  str
 end
 
 def product_inject(arr)
-    arr.inject {|acc,ele| acc * ele}
+    arr.inject {|acc, ele| acc * ele}
 end
