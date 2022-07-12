9b46b7753f3d6848c843827231efef939c0f51f5
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-07-12 11:41:32 -0700

    updation

--- Perilous_Procs/Perilous_Procs.rb
+++ Perilous_Procs/Perilous_Procs.rb
@@ -425,36 +425,36 @@ def picky_procipher(sent, hash)
   new_arr.join(" ")
 end
 
-is_yelled = Proc.new { |s| s[-1] == '!' }
-is_upcase = Proc.new { |s| s.upcase == s }
-contains_a = Proc.new { |s| s.downcase.include?('a') }
-make_question = Proc.new { |s| s + '???' }
-reverse = Proc.new { |s| s.reverse }
-add_smile = Proc.new { |s| s + ':)' }
+# is_yelled = Proc.new { |s| s[-1] == '!' }
+# is_upcase = Proc.new { |s| s.upcase == s }
+# contains_a = Proc.new { |s| s.downcase.include?('a') }
+# make_question = Proc.new { |s| s + '???' }
+# reverse = Proc.new { |s| s.reverse }
+# add_smile = Proc.new { |s| s + ':)' }
 
-p picky_procipher('he said what!',
-    is_yelled => make_question,
-    contains_a => reverse
-) # "he dias what!???"
+# p picky_procipher('he said what!',
+#     is_yelled => make_question,
+#     contains_a => reverse
+# ) # "he dias what!???"
 
-p picky_procipher('he said what!',
-    contains_a => reverse,
-    is_yelled => make_question
-) # "he dias !tahw"
+# p picky_procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => make_question
+# ) # "he dias !tahw"
 
-p picky_procipher('he said what!',
-    contains_a => reverse,
-    is_yelled => add_smile
-) # "he dias !tahw"
+# p picky_procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => add_smile
+# ) # "he dias !tahw"
 
-p picky_procipher('stop that taxi now',
-    is_upcase => add_smile,
-    is_yelled => reverse,
-    contains_a => make_question
-) # "stop that??? taxi??? now"
+# p picky_procipher('stop that taxi now',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "stop that??? taxi??? now"
 
-p picky_procipher('STOP that taxi!',
-    is_upcase => add_smile,
-    is_yelled => reverse,
-    contains_a => make_question
-) # "STOP:) that??? !ixat"
+# p picky_procipher('STOP that taxi!',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "STOP:) that??? !ixat"
